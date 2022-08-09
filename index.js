d7ae1c86e62ba9c9a9f214a1f79138937ff69a11
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-08-09 09:52:56 +0530

    Trying to auto authenticate

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+require('dotenv').config();
+// To get the authcode from the lib's logs
+console.info = m => {
+   if (m.startsWith("To sign in, use a web browser")) {
+      const authCode = /code (\w{8})/.exec(m)[1];
+      return authenticateLogin(authCode);
+   }
+   console.log(m);
+}
+
 const mineflayer = require('mineflayer');
 const config = require('./settings.json');
-require('dotenv').config();
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello Ji');
+});
+server.listen(8080);
+
 
 const noRestart = "no-restart";
 const opts = {
@@ -11,17 +28,18 @@ const opts = {
       version: "1.18.2",
 };
 
-function createBot() {
+const createBot = () => {
    if (!process.env.USER || !process.env.PASS || !process.env.SERVER_IP || !process.env.CHAT_PASS)
    	throw new Error("Pleave provide `USER`, `PASS`, `SERVER_IP` & `CHAT_PASS` env variables");
 
+   return;
    const bot = mineflayer.createBot(opts); 
    let lastLevel;
 
    bot.settings.colorsEnabled = false;
 
    bot.once('spawn', () => {
-      console.log(`[BotLog] Bot joined to the server\nCurrent level: ${bot.experience.level}`);
+      console.log(`[BotLog] Bot joined to the server`);
       opts['password'] = process.env.PASS;
       bot.chat(process.env.CHAT_PASS);
       lastLevel = bot.experience.level;
@@ -60,6 +78,55 @@ function createBot() {
    bot.on('error', (err) =>
       console.log(`[ERROR] ${err.message}`)
    );
+}
+/*CODE: //*[@id="otc"]
+
+EMAIL: //*[@id="i0116"]
+PASS: //*[@id="i0118"]
+
+NO: //*[@id="idBtn_Back"]
+
+WENT_WRONG_MESG: //*[@id="idDiv_Finish_Title"]
+// Something went wrong
+*/
+
+const CODE_XPATH = "//*[@id="otc"]";
+const EMAIL_XPATH = "//*[@id="i0116"]";
+const PASS_XPATH = "//*[@id="i0118"]";
+const NO_BUTTON_XPATH = "//*[@id="idBtn_Back"]";
+const STATUS_XPATH = "//*[@id="idDiv_Finish_Title"]";
+
+
+const authenticateLogin = async (authCode) => {
+   const link = "https://www.microsoft.com/link";
+   const puppeteer = require('puppeteer-core');
+   
+   const browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: null,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+   });
+
+   const page = await browser.newPage();
+   await page.goto(link, {
+      waitUntil: 'networkidle0'
+   });
+
+   // Enter code
+   await Promise.all([page.waitForSelector(CODE_XPATH), page.type(CODE_XPATH, authCode), page.keyboard.press('Enter')]);
+ 
+   // Enter email
+   await Promise.all([page.waitForSelector(EMAIL_XPATH), page.type(EMAIL_XPATH, process.env.USER), page.keyboard.press('Enter')]);
+   // Enter password
+   await Promise.all([page.waitForSelector(PASS_XPATH), page.type(PASS_XPATH, process.env.PASS), page.keyboard.press('Enter')]);
+   // Click "NO"
+   await Promise.all([page.waitForSelector(NO_BUTTON_XPATH), page.click(NO_BUTTON_XPATH)]);
+   // check if everything is fine
+   const element = await page.waitForSelector(STATUS_XPATH);
+   const status = await element.evaluate(e => e.textContent);
+   console.log("hello "+status);
+
+   await browser.close();
 }
 
 createBot();
