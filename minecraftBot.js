3c7838bb2de62ff057a25a774274a05192adb884
ChampionBuffalo <49675578+ChampionBuffalo1@users.noreply.github.com>
2022-08-10 08:05:36 +0530

    Update minecraftBot.js

--- minecraftBot.js
+++ minecraftBot.js
@@ -52,7 +52,10 @@ module.exports = class MinecraftBot {
         }
     }
     death = () => this.log.info(`Bot has been died and was respawned ${this.bot.entity.position}`);
-    kicked = reason => this.log.info( `Bot was kicked from the server. Reason: \n${reason})`);
+    kicked = reason => {
+        this.bot = null;
+        this.log.info( `Bot was kicked from the server. Reason: \n${reason})`);
+    }
     botDown = reason => {
         this.bot = null;
         if (reason !== this.noReconnect && this.config["auto-reconnect"]) {
